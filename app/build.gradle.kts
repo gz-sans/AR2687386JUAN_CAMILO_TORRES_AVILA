d40570cd17d55996a176a93a6e9143c6ade6f8be
juan camilo torres avila <114105095+gz-sans@users.noreply.github.com>
2023-09-06 11:10:19 -0500

    arreglando errores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.7.1")
 
     implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
 
