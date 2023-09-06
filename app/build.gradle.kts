8208a5d85dbf6528696bf9f2197f12baf18cb4a7
juan camilo torres avila <114105095+gz-sans@users.noreply.github.com>
2023-09-06 11:08:42 -0500

    Uptate copilesdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-
+    //add the Google services gradle plugin
+    id("com.google.gms.google-services")
+    //add hilt to dependency injection
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -31,11 +38,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -53,6 +60,20 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
+    //fire base
+    implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
+    //fire base
+    implementation("com.google.firebase-auth-ktx:22.1.1")
+    //fire base
+    implementation("com.google.firebase:firebase-firestore:24.7.1")
+
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
